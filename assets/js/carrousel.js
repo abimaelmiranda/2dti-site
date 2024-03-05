c9224dd6d826ccaf309392c9a46f91ee3189ac9d
m1randa04 <mirandaabimael57@gmail.com>
2024-03-05 17:18:49 -0300

    Seção clientes
    Criação e personalização da seção clientes

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -1,17 +1,42 @@
-const imgs = document.querySelector(`.carousel`);
-const img = document.querySelectorAll(`.carousel img`);
+(function () {
+    const imgs = document.querySelector(`.carousel`);
+    const img = document.querySelectorAll(`.carousel img`);
 
-let id = 0;
+    let id = 0;
 
-function carrousel() {
-    id++;
+    function carrouselSol() {
+        id++;
 
-    if(id > img.length - 1){
-        id = 0;
+        if (id > img.length - 1) {
+            id = 0;
+        }
+
+        imgs.style.transform = `translateX(${-id * 30}rem)`
+
     }
 
-    imgs.style.transform = `translateX(${-id * 30}rem)`
-    
-}
+    setInterval(carrouselSol, 1800);
+})();
 
-setInterval(carrousel, 1800);
+(function () {
+
+        const imgs = document.querySelector(`.gallery`);
+        const img = document.querySelectorAll(`.gallery img`);
+
+        let id = 0;
+
+        function carrouselClientes() {
+            id++;
+
+            if (id > img.length - 1) {
+                id = 0;
+            }
+
+            imgs.style.transform = `translateX(${-id * 50}rem)`
+
+        }
+
+    setInterval(carrouselClientes, 1800);
+
+
+})();
